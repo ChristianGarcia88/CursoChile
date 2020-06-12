c2b4a3c814cd0517c6870b3d04fe0dd0ec78094c
x <guillep890329@gmail.com>
2020-06-12 17:14:49 -0500

    Curso chile

---
+++ src/main/java/com/example/CursoChile/CursoChileApplication.java
@@ -0,0 +1,9 @@
+import com.example.CursoChile.*;
+@SpringBootApplication
+public class CursoChileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoChileApplication.class, args);
+	}
+
+}
